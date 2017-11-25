bf9ce7708beb0e465ea137f6512ec9e79c913e45
Sarah Morris <sarah.morris9@gmail.com>
2017-11-25 17:18:12 -0600

    Completed challenge

--- boybands.js
+++ boybands.js
@@ -1,14 +1,6 @@
-var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var bands = ["BTS", "Pretty Much", "5 Seconds of Summer", "The 1975", "One Direction"];
 var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
-// The number of loops to perform (what if the array changes?)
-var loopCount = 5;
-
-// Keep track of which band we're on in the loop
-var currentBand = "";
-
-// Keep track of which veggie we're on in the loop
-var currentVeggie = "";
 
 // Get a reference to the appropriate DOM element for bands
 var bandElement = document.getElementById('boy-bands');
@@ -17,13 +9,10 @@ var bandElement = document.getElementById('boy-bands');
 var veggieElement = document.getElementById('vegetables');
 
 // Start looping
-for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
-  
-  // Add the band names into the correct <div>
-  currentBand = ???;
+for (var i = 0; i < bands.length; i += 1) {
+  bandElement.innerHTML += "<p>" + bands[i] + "</p>";
+}
 
-  
-  // Add the veggie names into the correct <div>
-  currentVeggie = ???;
-
+for (var i = 0; i < vegetables.length; i += 1) {
+  veggieElement.innerHTML += "<p>" + vegetables[i] + "</p>";
 }
